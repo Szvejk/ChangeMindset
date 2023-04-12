e4e7c14408b6fb21df296d8688595e45fdef54a5
Sara Szwejkowska <saraszw@op.pl>
2023-04-12 22:40:24 +0200

    changes in css styles and created B1-B2 exercises lvl

--- src/components/pages/Grammar/Levels/Middle/Middle.tsx
+++ src/components/pages/Grammar/Levels/Middle/Middle.tsx
@@ -4,8 +4,107 @@ import styles from '../Middle/middle.module.css'
 
 const middle = () => {
   return (
-    <div>
-      
+    <div className={styles.wrapperMiddle}>
+       
+      	<div className={styles.zad1middle}>
+				<h5 className={styles.titleExercise}>
+					I ___ anything about the night of the accident.
+				</h5>
+				<p>a. don't remember</p>
+				<p>b.'m not remembering</p>
+				<p>c.wasn't remembering</p>
+			</div>
+      <button className={styles.checkAnswers1}>Sprawdź odpowiedź</button>
+		<div className="answersEx1"></div>
+
+
+
+			<div className={styles.zad2middle}>
+				<h5 className={styles.titleExercise}>
+			Since they started, they ___ social media to promote their services.
+				</h5>
+				<p>a. use</p>
+				<p>b. were using</p>
+				<p>c. have been using</p>
+			</div>
+      <button className={styles.checkAnswers2}>Sprawdź odpowiedź</button>
+		<div className="answersEx2"></div>
+
+
+
+			<div className={styles.zad3middle}>
+				<h5 className={styles.titleExercise}>
+			He stopped talking because nobody ___
+				</h5>
+				<p>a. listens</p>
+				<p>b. had listened</p>
+				<p>c. was listening</p>
+			</div>
+      <button className={styles.checkAnswers3}>Sprawdź odpowiedź</button>
+		<div className="answersEx3"></div>
+
+
+
+			<div className={styles.zad4middle}>
+				<h5 className={styles.titleExercise}>
+			In five years, 60% of the world's population ___ the internet.
+				</h5>
+				<p>a. are going to use</p>
+				<p>b. will use</p>
+				<p>c. are using</p>
+			</div>
+      <button className={styles.checkAnswers4}>Sprawdź odpowiedź</button>
+
+
+
+		<div className="answersEx4"></div>
+			<div className={styles.zad5middle}>
+				<h5 className={styles.titleExercise}>
+				We ___ for nearly an hour when we realised that we were totally lost.
+				</h5>
+				<p>a. had been walking</p>
+				<p>b. had walked</p>
+				<p>c. were walking</p>
+			</div>
+      <button className={styles.checkAnswers4}>Sprawdź odpowiedź</button>
+
+
+		<div className="answersEx4"></div>
+			<div className={styles.zad6middle}>
+				<h5 className={styles.titleExercise}>
+			A: "I'm really thirsty." B:"I ___ you some water"
+				</h5>
+				<p>a. 'm going to give</p>
+				<p>b. will be giving </p>
+				<p>c. 'll give</p>
+			</div>
+      <button className={styles.checkAnswers4}>Sprawdź odpowiedź</button>
+
+    
+		<div className="answersEx4"></div>
+			<div className={styles.zad7middle}>
+				<h5 className={styles.titleExercise}>
+				If you start repairing the car now, do you think you___by the end of the day.
+				</h5>
+				<p>a. will be finishing</p>
+				<p>b. are going to finish</p>
+				<p>c. will have finished</p>
+			</div>
+      <button className={styles.checkAnswers4}>Sprawdź odpowiedź</button>
+		<div className="answersEx4"></div>
+
+
+			<div className={styles.zad8middle}>
+				<h5 className={styles.titleExercise}>
+				When it was time to go home, we decided to take a taxi because we ___ too many beers.
+				</h5>
+				<p>a. were drinking</p>
+				<p>b. had been drinking</p>
+				<p>c. had drunk</p>
+			</div>
+      <button className={styles.checkAnswers4}>Sprawdź odpowiedź</button>
+		<div className="answersEx4"> c</div>
+    
     </div>
   )
 }
